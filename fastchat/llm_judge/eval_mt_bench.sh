0bb3516d5404abb855f6cf3f771def3d5785ffe7
Bo Ren <boren@microsoft.com>
2024-10-10 21:01:19 +0000

    update script

--- fastchat/llm_judge/eval_mt_bench.sh
+++ fastchat/llm_judge/eval_mt_bench.sh
@@ -3,15 +3,18 @@
 set -x
 model_path=microsoft/Phi-3.5-mini-instruct
 model_id=Phi-3.5-mini-instruct
+# model_id=Phi-Omni_20241009
+model_id=Phi-Omni-Audio_20241010
 work_dir=dev_data
-models=(gpt-4 gpt-3.5-turbo llama-13b $model_id)
+models=(gpt-4 gpt-3.5-turbo Phi-3.5-mini-instruct $model_id)
 
 
+# echo gen_model_answer.py  --work-dir ${work_dir} --model-path ${model_path} --model-id ${model_id}
 # python gen_model_answer.py  --work-dir ${work_dir} --model-path ${model_path} --model-id ${model_id}
 
 # export AZURE_OPENAI_API_KEY=XXXXXX  # set the OpenAI API key
-# python gen_judgment.py --work-dir ${work_dir} --model-list $model_id --judge-model gpt-4
+python gen_judgment.py --work-dir ${work_dir} --model-list $model_id --judge-model gpt-4
 
 python show_result.py --work-dir ${work_dir} --model-list  ${models[@]}
 
-# python qa_browser.py --work-dir ${work_dir} --share
+python qa_browser.py --work-dir ${work_dir} --share
