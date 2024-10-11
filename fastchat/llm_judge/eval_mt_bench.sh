1af12bc31d6450e421dbe042d87605a05d500c26
Bo Ren <boren@microsoft.com>
2024-10-11 18:25:46 +0000

    update script

--- fastchat/llm_judge/eval_mt_bench.sh
+++ fastchat/llm_judge/eval_mt_bench.sh
@@ -6,25 +6,24 @@ work_dir=dev_data
 # model_id=Phi-3.5-mini-instruct
 # echo gen_model_answer.py  --work-dir ${work_dir} --model-path ${model_path} --model-id ${model_id}
 # python gen_model_answer.py  --work-dir ${work_dir} --model-path ${model_path} --model-id ${model_id}
-base_models=(
-    gpt-4
-    gpt-3.5-turbo
-    Phi-3.5-mini-instruct
-    )
-new_models=(
+models=(
+    # gpt-4
+    # gpt-3.5-turbo
+    # Phi-3.5-mini-instruct
     # Phi-Omni_20241009
     # Phi-Omni-Audio_20241010
-    Phi3.5-Omni-audio_20241010
-    Phi3.5-Omni-text_20241010
+    # Phi3.5-Omni-audio_20241010
+    # Phi3.5-Omni-text_20241010
     # Phi3.5-Omni-text-v0_20241010_221331
     # Phi3.5-Omni-audio-v0_20241010_222909
+    Phi3.5-internal_20241011_182438
     )
     
-models=(${base_models[@]} ${new_models[@]})
 
-for model_id in ${new_models[@]}; do
+for model_id in ${models[@]}; do
     python gen_judgment.py --work-dir ${work_dir} --model-list $model_id --judge-model gpt-4
 done
+
 
 python show_result.py --work-dir ${work_dir} --model-list  ${models[@]}
 
